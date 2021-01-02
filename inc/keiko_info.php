b0950603aee7358cadf7edb5feade30bd9d2d3ae
MitsutakaKikuchi <kiku.mitsu.yoshi.taka@gmail.com>
2021-01-03 00:53:13 +0900

    a

--- inc/keiko_info.php
+++ inc/keiko_info.php
@@ -53,17 +53,6 @@
             </p>
         </div>
 
-        <div class="keiko-info">
-            <img src="images/keiko-tokyo.jpg" alt="" />
-            <h3>〇〇 お稽古場</h3>
-            <p>
-                [住所]<br /><span>テキスト入力 </span><br />
-                [TEL]<br /><span>テキスト入力</span><br />
-                [アクセス]<br />
-                <span>
-                テキスト入力 テキスト入力 テキスト入力 テキスト入力 テキスト入力 テキスト入力 <br />テキスト入力 テキスト入力 テキスト入力 テキスト入力 テキスト入力 テキスト入力 <br /> テキスト入力 テキスト入力 テキスト入力 テキスト入力 テキスト入力 テキスト入力 <br />
-                </span>
-            </p>
-        </div>
+
     </container>
 </div>
